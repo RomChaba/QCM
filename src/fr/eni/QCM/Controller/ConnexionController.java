ac1fafbff4922a6d6bd4e535b24c7c5a44472d38
RomChaba <chabassier.r@gmail.com>
2017-06-26 16:11:38 +0200

    test

--- src/fr/eni/QCM/Controller/ConnexionController.java
+++ src/fr/eni/QCM/Controller/ConnexionController.java
@@ -32,28 +32,6 @@ public class ConnexionController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		try {
-			Connection conn = AccesBase.recupererConnexionJDBC();
-			Statement statement = conn.createStatement();
-			/* Exécution d'une requête de lecture */
-			ResultSet resultat = statement.executeQuery( "SELECT * FROM Candidat;" );
-			while ( resultat.next() ) {
-			    int idUtilisateur = resultat.getInt( "idUtilisateur" );
-			    int idType = resultat.getInt( "idType" );
-			    response.getWriter().append("Served at: "+idUtilisateur+" type:"+idType+"<br>");
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		/* Création de l'objet gérant les requêtes */
-		
-		// TODO Auto-generated method stub
-		
-		
-		
-		
 	}
 
 	/**
